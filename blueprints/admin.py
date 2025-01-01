f10c63cf5e224dfd68427dc93db55b5a12f5a45e
Admin <atefepournasrolah@gmail.com>
2025-01-01 16:48:12 +0330

    create file config and first file html

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, session, redirect, render_template
+import config
 app = Blueprint("admin", __name__)
+
+@app.route('/admin/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+
+        if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD:
+            session['admin_login'] = username
+            return redirect("/admin/dashboard")
+        else:
+            return redirect("/admin/login")
+
+    else:
+        return render_template("admin/login.html")
